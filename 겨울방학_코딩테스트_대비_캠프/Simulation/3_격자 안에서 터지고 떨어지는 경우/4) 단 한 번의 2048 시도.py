48d1ec8e5d9491d5d71358466c3846aa5e8d5228
Jae_Yu <ryooju0114@gmail.com>
2021-02-01 00:05:14 +0900

    2048-리팩토링필요

---
+++ 겨울방학_코딩테스트_대비_캠프/Simulation/3_격자 안에서 터지고 떨어지는 경우/4) 단 한 번의 2048 시도.py
@@ -0,0 +1,177 @@
+'''
+코드 리팩토링 필요
+'''
+
+grid = [
+    list(map(int,input().split()))
+    for _ in range(4)
+]
+
+dir = input()
+
+dirs = ['L','R','U','D']
+dx,dy = [0,0,-1,1],[-1,1,0,0]
+
+dir_idx = dirs.index(dir)
+
+result_grid = [
+    [0 for _ in range(4)]
+    for _ in range(4)
+]
+
+temp_grid = []
+
+
+if dir == 'L':
+    for i in range(4):
+        temp = []
+        origin_num = False
+        compare_num = False
+        for j in range(4):
+            if grid[i][j] == 0: # 비어있는 공간
+                continue
+
+            if not origin_num:
+                origin_num = grid[i][j]
+            else:
+                if not compare_num:
+                    compare_num = grid[i][j]
+
+                if origin_num == compare_num:
+                    temp.append(origin_num + compare_num)
+                    origin_num = False
+                    compare_num = False
+                else:
+                    temp.append(origin_num)
+                    origin_num = compare_num
+                    compare_num = False
+
+        if origin_num : temp.append(origin_num)
+        if compare_num : temp.append(compare_num)
+
+        temp_grid.append(temp)
+
+    for i in range(4):
+        for j in range(4):
+            if temp_grid[i]:
+                result_grid[i][j] = temp_grid[i].pop(0)
+            else:
+                break
+
+if dir == 'R':
+    for i in range(4):
+        temp = []
+        origin_num = False
+        compare_num = False
+        for j in range(3,-1,-1):
+            if grid[i][j] == 0: # 비어있는 공간
+                continue
+
+            if not origin_num:
+                origin_num = grid[i][j]
+            else:
+                if not compare_num:
+                    compare_num = grid[i][j]
+
+                if origin_num == compare_num:
+                    temp.append(origin_num + compare_num)
+                    origin_num = False
+                    compare_num = False
+                else:
+                    temp.append(origin_num)
+                    origin_num = compare_num
+                    compare_num = False
+
+        if origin_num : temp.append(origin_num)
+        if compare_num : temp.append(compare_num)
+
+        temp_grid.append(temp)
+
+    for i in range(4):
+        for j in range(3,-1,-1):
+            if temp_grid[i]:
+                result_grid[i][j] = temp_grid[i].pop(0)
+            else:
+                break
+
+if dir == 'U':
+    for j in range(4):
+        temp = []
+        origin_num = False
+        compare_num = False
+        for i in range(4):
+            if grid[i][j] == 0: # 비어있는 공간
+                continue
+
+            if not origin_num:
+                origin_num = grid[i][j]
+            else:
+                if not compare_num:
+                    compare_num = grid[i][j]
+
+                if origin_num == compare_num:
+                    temp.append(origin_num + compare_num)
+                    origin_num = False
+                    compare_num = False
+                else:
+                    temp.append(origin_num)
+                    origin_num = compare_num
+                    compare_num = False
+
+        if origin_num : temp.append(origin_num)
+        if compare_num : temp.append(compare_num)
+
+        temp_grid.append(temp)
+
+    for j in range(4):
+        for i in range(4):
+            if temp_grid[j]:
+                result_grid[i][j] = temp_grid[j].pop(0)
+            else:
+                break
+
+if dir == 'D':
+    for j in range(4):
+        temp = []
+        origin_num = False
+        compare_num = False
+        for i in range(3,-1,-1):
+            if grid[i][j] == 0: # 비어있는 공간
+                continue
+
+            if not origin_num:
+                origin_num = grid[i][j]
+            else:
+                if not compare_num:
+                    compare_num = grid[i][j]
+
+                if origin_num == compare_num:
+                    temp.append(origin_num + compare_num)
+                    origin_num = False
+                    compare_num = False
+                else:
+                    temp.append(origin_num)
+                    origin_num = compare_num
+                    compare_num = False
+
+        if origin_num : temp.append(origin_num)
+        if compare_num : temp.append(compare_num)
+
+        temp_grid.append(temp)
+
+    for j in range(4):
+        for i in range(3,-1,-1):
+            if temp_grid[j]:
+                result_grid[i][j] = temp_grid[j].pop(0)
+            else:
+                break
+
+
+
+for i in range(4):
+    for j in range(4):
+        print(result_grid[i][j],end=' ')
+    print()
+
+
+
