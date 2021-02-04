3249ba5029a9ba47dc19b04e0d205ea207aca18f
Jae_Yu <ryooju0114@gmail.com>
2021-02-04 12:10:44 +0900

    문제수정 후 풀이

--- 겨울방학_코딩테스트_대비_캠프/Simulation/5_격자 안에서 여러 객체를 이동/2) 숫자의 순차적 이동.py
+++ 겨울방학_코딩테스트_대비_캠프/Simulation/5_격자 안에서 여러 객체를 이동/2) 숫자의 순차적 이동.py
@@ -7,30 +7,7 @@
 
 질의 게시판에 올림.
 
-tk 3번
-9 79
-15 38 19 22 4 60 7 36 5
-35 74 1 18 24 53 48 57 16
-30 77 14 70 52 12 39 20 37
-46 27 72 6 54 71 9 32 65
-50 42 17 13 2 55 10 44 41
-79 31 11 76 47 33 8 45 29
-61 68 34 75 26 28 25 59 43
-66 78 3 56 63 80 23 0 62
-64 51 73 49 40 58 67 21 69
-
-
-40 30 1 50 32 34 8 69 13
-45 39 76 81 77 73 5 71 70
-75 14 29 16 52 58 54 62 22
-46 11 68 4 41 25 80 7 63
-31 12 47 74 42 38 78 18 64
-3 48 15 24 17 56 2 55 59
-10 79 36 66 51 57 19 33 9
-21 26 49 67 61 23 20 0 6
-28 43 37 60 35 53 65 72 27
-
-
+-> 문제 수정되었음 ( Passed )
 '''
 
 import itertools
@@ -47,51 +24,24 @@ dirs = list(itertools.permutations(dirs,2))
 dirs.append((-1,-1))
 dirs.append((1,1))
 
-dx,dy = [1,-1,0,0],[0,0,-1,1]
-
 def in_range(x,y):
     return 0 <= x < n and 0 <= y < n
 
 def change_nums(x,y):
     max_x,max_y = x,y
     max_val = 0
-    # print('-----------------------')
-    # print('바뀌기 전 --------')
-    # for i in range(n):
-    #     for j in range(n):
-    #         print('%3d'%(grid[i][j]), end=' ')
-    #     print()
-    #
-    # print()
-    #
-    # print(grid[x][y],'의 주위 숫자들')
-    for i in range(4):
-        new_x, new_y = x + dx[i], y + dy[i]
-        if in_range(new_x, new_y):
-            # print(grid[new_x][new_y],end=' ')
+
+    # 8방향 탐색
+    for dir_x,dir_y in dirs:
+        new_x,new_y = x+dir_x, y+dir_y
+        if in_range(new_x,new_y):
+
             if max_val < grid[new_x][new_y]:
                 max_val = grid[new_x][new_y]
-                max_x, max_y = new_x, new_y
+                max_x, max_y = new_x,new_y
 
-    # for dir_x,dir_y in dirs:
-    #     new_x,new_y = x+dir_x, y+dir_y
-    #     if in_range(new_x,new_y):
-    #         # print(grid[new_x][new_y],end=' ')
-    #         if max_val < grid[new_x][new_y]:
-    #             max_val = grid[new_x][new_y]
-    #             max_x, max_y = new_x,new_y
-    # print('----------')
-    # print(grid[max_x][max_y],'선택-----')
-    # 인접한 위치중 최대값과 교환
     grid[x][y],grid[max_x][max_y] = grid[max_x][max_y],grid[x][y]
 
-
-    # for i in range(n):
-    #     for j in range(n):
-    #         print('%3d'%(grid[i][j]), end=' ')
-    #     print()
-    #
-    # print()
     return
 
 # m번반복
@@ -99,7 +49,6 @@ for _ in range(m):
 
     # 1에서 n**2까지
     for num in range(n**2+1):
-        #print('현재 num = ',num)
         break_loop = False
 
         for i in range(n):
