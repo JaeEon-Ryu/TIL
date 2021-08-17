eef1d7919ca9946981b729bca80e13f1ee8eec67
Jae_Yu <ryooju0114@gmail.com>
2021-08-17 23:21:04 +0900

    nodejs socket.io로 채팅 서비스 만들기

--- NodeJS_VueJS/index.js
+++ NodeJS_VueJS/index.js
@@ -9,6 +9,7 @@ const testJson = require('./test/test.json');
 const pool = new Pool();
 
 app.use(express.static('public')); // public 디렉토리를 static으로
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -32,6 +33,53 @@ app.get('/dbtest/:user', (req, res) => {
 
 const server = app.listen(7000, function(){
     console.log("Express's started on port 7000");
+});
+
+const io = require('socket.io')(server, {
+    log: false,
+    origins: '*:*',
+    pingInterval: 3000,
+    pingTimeout: 5000
+});
+
+io.sockets.on('connection', (socket, opt) => {
+    socket.emit('message', {msg: 'Welcome ' + socket.id});
+
+    util.log("connection>>",socket.id, socket.handshake.query)
+
+    socket.on('join',function(roomId, fn) {
+        socket.join(roomId, function() {
+            util.log("Join",roomId,Object.keys(socket.rooms))
+            if (fn)
+                fn();
+        });
+    })
+
+    socket.on('leave',function(roomId, fn) {
+        socket.leave(roomId,function(){
+            if (fn)
+                fn();
+        });
+    });
+
+    socket.on('rooms',function(fn) {
+        if (fn)
+            fn(Object.keys(socket.rooms));
+    });
+
+    socket.on('message', (data, fn) => {
+        util.log("message>>", data.msg, Object.keys(socket.rooms));
+        fn(data.msg);
+    });
+
+    socket.on('disconnecting',function(data) {
+        util.log("disconnecting>>", socket.id, Object.keys(socket.rooms))
+    })
+
+    socket.on('disconnect',function(data) {
+        util.log("disconnect>>", socket.id, Object.keys(socket.rooms))
+    })
+
 });
 
 
